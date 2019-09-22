3ff4c9586118973c547492d5e23de4c9809ebde1
Roei Bracha <roei635@gmail.com>
2019-09-22 22:21:05 +0300

    adding a tabs navigator

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { View, Text, StyleSheet , Button } from 'react-native';
-
-const MealDeatailsScreen = props => {
+import { MEALS } from '../data/dummy-data';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import HeaderButton from '../components/HeaderButton'
+const MealDetailsScreen = props => {
+    const mealId = props.navigation.getParam('mealId')
+    const selectedMeal = MEALS.find((meal)=>meal.id === mealId)
     return (
         <View style={styles.screen}>
             <Text> the meal details screen</Text>
+            <Text>{selectedMeal.title}</Text>
             <Button title="Back" onPress={()=>{props.navigation.popToTop()}} />
         </View>
     )
+}
+
+MealDetailsScreen.navigationOptions = (navigationData) => {
+    const selectedMeal = MEALS.find((meal)=>meal.id === navigationData.navigation.getParam('mealId'))
+    return {
+        headerTitle: selectedMeal.title,
+        headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item title={'favorite'} iconName={'ios-star'} onPress={()=>{c)}}/>
+        </HeaderButtons>
+    }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +33,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default MealDeatailsScreen
+export default MealDetailsScreen
